bcd754df5b96b7b414b03553931010912a87b087
Ирина Князева <irina2003608@gmail.com>
2025-07-07 16:28:38 +0300

    сохранение 07.07.2025

---
+++ DZ-04.07.25/DZ-04.07.25/Book.cs
@@ -0,0 +1,46 @@
+﻿namespace DZ_04._07._25;
+
+internal class Book //создание класса с полями
+{
+    public string title;  //поля
+    public string author;
+    public int year;
+    public bool IsAvailable;
+
+    /// <summary>
+    /// Метод, который выводит инфу на консоль
+    /// </summary>
+    public void DisplayInfo() 
+    {
+        Console.WriteLine($"\nНазвание книги: {title} Автор: {author} Год издания: {year} Доступна ли книга: {IsAvailableText()}");
+    }
+
+    public void Berrow()
+    {
+        if (IsAvailable == false)
+        {
+            Console.WriteLine("Книга была выдана");
+        }
+    }
+
+    public void Return()
+    { 
+        if (IsAvailable == true)
+        {
+            Console.WriteLine("Книга в библиотеке");
+        }
+    }
+
+   public void YearBook()
+    {
+        if (year == 0)
+        {
+            Console.WriteLine("Неизвестный год издания");
+        }
+    }
+
+    private string IsAvailableText()
+    {
+        return IsAvailable ? "Книга в библиотеке" : "Книга была выдана";
+    }
+}
